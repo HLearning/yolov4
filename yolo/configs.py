e8356e130feff2cdef0469c009679d69807972c8
HLearning <544705740@qq.com>
2020-09-16 16:29:20 +0800

    iou

--- yolo/configs.py
+++ yolo/configs.py
@@ -4,3 +4,25 @@
 # @Date            : 2020-05-01
 # ========================================
 
+
+class Config(object):
+    batch = 64
+    # 输入图片尺寸
+    width = 608
+    height = 608
+    channels = 3
+    learning_rate = 0.0013
+
+    # 锚点
+    anchors = [[12, 16], [19, 36], [40, 28], [36, 75], [76, 55], [72, 146], [142, 110], [192, 243], [459, 401]]
+    # 锚点的个数
+    num_anchors = 9
+    anchors_mask = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    # coco数据集的类别数
+    num_classes = 80
+
+    # 副样本产生的阈值
+    ignore_thresh = 0.7
+    # loss函数
+    iou_loss = 'ciou'
+    beta_nms = 0.6
--- yolo/utils/iou_utils.py
+++ yolo/utils/iou_utils.py
@@ -5,37 +5,59 @@
 # ========================================
 
 import tensorflow as tf
+import math
 
 
-def box_iou(b1, b2):
+def tf_iou(tensor1, tensor2, mode="iou"):
     """
-    计算box的iou
-    :param b1: tensor, shape=(i1,...,iN, 4), xywh
-    :param b2: tensor, shape=(j, 4), xywh
-    :return:
+    计算 iou
+    :param tensor1: shape=[N, 4] 4: x, y, w, h
+    :param tensor2: shape=[N, 4] 4: x, y, w, h
+    :param mode: 使用的iou的类型， [iou, giou, diou, ciou]
+    :return: iou
     """
-    # Expand dim to apply broadcasting.
-    b1 = tf.expand_dims(b1, -2)
-    b1_xy = b1[..., :2]
-    b1_wh = b1[..., 2:4]
-    b1_wh_half = b1_wh/2.
-    b1_mins = b1_xy - b1_wh_half
-    b1_maxes = b1_xy + b1_wh_half
+    tensor1 = tf.cast(tensor1, tf.float32)
+    tensor2 = tf.cast(tensor2, tf.float32)
 
-    # Expand dim to apply broadcasting.
-    b2 = tf.expand_dims(b2, 0)
-    b2_xy = b2[..., :2]
-    b2_wh = b2[..., 2:4]
-    b2_wh_half = b2_wh/2.
-    b2_mins = b2_xy - b2_wh_half
-    b2_maxes = b2_xy + b2_wh_half
+    tensor1_xy = tensor1[..., 0:2]
+    tensor1_wh = tensor1[..., 2:4]
+    tensor1_x1y1 = tensor1_xy - tensor1_wh * 0.5
+    tensor1_x2y2 = tensor1_xy + tensor1_wh * 0.5
 
-    intersect_mins = tf.maximum(b1_mins, b2_mins)
-    intersect_maxes = tf.minimum(b1_maxes, b2_maxes)
+    tensor2_xy = tensor2[..., 0:2]
+    tensor2_wh = tensor2[..., 2:4]
+    tensor2_x1y1 = tensor2_xy - tensor2_wh * 0.5
+    tensor2_x2y2 = tensor2_xy + tensor2_wh * 0.5
+
+    intersect_mins = tf.maximum(tensor1_x1y1, tensor2_x1y1)
+    intersect_maxes = tf.minimum(tensor1_x2y2, tensor2_x2y2)
     intersect_wh = tf.maximum(intersect_maxes - intersect_mins, 0.)
     intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
-    b1_area = b1_wh[..., 0] * b1_wh[..., 1]
-    b2_area = b2_wh[..., 0] * b2_wh[..., 1]
-    iou = intersect_area / (b1_area + b2_area - intersect_area)
 
-    return iou
+    tensor1_area = tensor1_wh[..., 0] * tensor1_wh[..., 1]
+    tensor2_area = tensor2_wh[..., 0] * tensor2_wh[..., 1]
+    union_area = tensor1_area + tensor2_area - intersect_area
+    iou = intersect_area / union_area
+    if mode == "iou":
+        return iou
+
+    enclose_x1y1 = tf.minimum(tensor1_x1y1, tensor2_x1y1)
+    enclose_x2y2 = tf.maximum(tensor1_x2y2, tensor2_x2y2)
+    enclose_wh = tf.maximum(enclose_x2y2 - enclose_x1y1, 0.)
+    enclose_area = enclose_wh[..., 0] * enclose_wh[..., 1]
+    giou = iou - (enclose_area - union_area) / enclose_area
+    if mode == "giou":
+        return giou
+
+    dd = tf.reduce_sum(tf.square(tensor1_xy - tensor2_xy), axis=-1)
+    cc = tf.reduce_sum(tf.square(enclose_wh), axis=-1)
+    diou = iou - dd / cc
+    if mode == "diou":
+        return diou
+
+    v = tf.constant(4 / math.pi ** 2) * tf.square(tf.math.atan(tensor1_wh[..., 0] / tensor1_wh[..., 1]) \
+                                                   - tf.math.atan(tensor2_wh[..., 0] / tensor2_wh[..., 1]), axis=-1)
+    alpha = v / (1 - iou + v)
+    ciou = diou - alpha * v
+    if mode == "ciou":
+        return ciou
